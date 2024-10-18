a78b5c6a1bd48d5c6bc5bf0fa770b8c77f455599
Dinidu-Lochana <24.dinidulochana@gmail.com>
2024-10-18 23:55:31 +0530

    Add and get

--- backend/routes/StudentRoutes.js
+++ backend/routes/StudentRoutes.js
@@ -2,3 +2,39 @@ const router = require("express").Router();
 
 let Student = require("../modules/student");
 
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+    const contactNumber = req.body.contactNumber;
+
+    const newStudent = new Student({
+        name,
+        age,
+        gender,
+        contactNumber
+    })
+
+    newStudent.save().then(()=>{
+        res.json("Student Added");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
+router.route("/").get((req,res)=>{
+    Student.find().then((students)=>{
+        res.json(students)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+})
+
+router.route("/update/:id").put((req,res)=>{
+    
+})
+
+
+module.exports = router;
